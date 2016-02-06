9c06241dc96e00ec0d9c38110c2267e7fd4f82af
Rene Springman <info@rene-springmann.de>
2016-02-06 19:05:14 +0100

    first visualisation

--- js/game.js
+++ js/game.js
@@ -5,8 +5,10 @@
      */
     window.game = window.game || {};
 
-    game.dimensions = {x : 30, y:30};
+    game.dimensions = {x : 30, y : 30};
     game.playground = null;
+    game.cells = {};
+    game.debug = false;
 
     game.init = function(playground){
         this.playground = playground;
@@ -15,13 +17,37 @@
 
     game.drawPlayground = function(){
         for (y = 0; y < this.dimensions.y; y++) {
-            var row = document.createElement("DIV");
-            console.debug(row);
-            row.className('playground-row').data('row-id', y);
+            var rowElement = $('<div>', {class: 'playground-row'});
+            this.playground.append(rowElement);
+            var row = $(rowElement);
+            row.data('row-id', y);
+            this.cells[y] = {};
             for (x = 0; x < this.dimensions.x; x++) {
-                //row.appendCh('<div class="playground-cell">'  + (x) + '-' + (y) +  '</div>');
+                var cellElement = $('<div>', {class: 'playground-cell', id: 'cell-' + y + '-' + x});
+                row.append(cellElement);
+                var cell = $(cellElement);
+                cell.data('row-id', y).data('cell-id', x).data('alive', false);
+                if(this.debug) cell.html(x + '-' + y);
+                this.cells[y][x]= false;
             }
-            this.playground.append(row);
+        }
+        //console.debug(this.cells);
+    };
+
+    game.refreshPlayground = function(){
+        this.cells[7][9]= true;
+        this.cells[25][10]= true;
+        this.cells[13][10]= true;
+        this.cells[2][23]= true;
+        for (y = 0; y < this.dimensions.y; y++) {
+            for (x = 0; x < this.dimensions.x; x++) {
+                if (this.cells[y][x]) {
+                    this.playground.find('#cell-' + y + '-' + x).addClass('alive', 400, 'easeInOutQuart');
+                }
+                else {
+                    this.playground.find('#cell-' + y + '-' + x).removeClass('alive');
+                }
+            }
         }
     };
 
